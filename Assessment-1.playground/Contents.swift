b79380a16f4bf39e39452494f34452c346eaccb6
JimCampagno <jimcampagno@gmail.com>
2017-02-12 21:36:06 -0500

    Initial Commit

---
+++ Assessment-1.playground/Contents.swift
@@ -0,0 +1,82 @@
+//: # Assessment
+//: ![](CuteCorgi.JPG)
+//: **1.** Create a new type representing a Dog. It should have two properties. One should represent the dogs name and another representing whether or not the dog has to pee. Create this new type below.
+
+
+
+
+// write your code here
+
+
+
+
+//: ---
+/*: Thing
+ **2.** There are various types of dog that exist. For this example, we'll only name a few:
+ * Pug
+ * Beagle
+ * Poodle
+ * Boxer
+ 
+How can we best represent this current scenario in code? What type would be best to use to handle all of these options? **BONUS:** Create this new type in code that represnts all of these options.
+ */
+
+
+
+
+// write your code here (not required for this question)
+
+
+
+
+//: ---
+//: **3.** Create two new types. The first type should be called Size and it should have two properties. Its properties should be called height and width. Create this custom type as a struct. Before you begin, are you required to create an initializer for this soon to be made custom type? Create another type called Point which has two properties. Its two properties should be called x and y. **Note**: Both Size and Point should able to change its properties to equal other values.
+
+
+
+
+// write your code here
+
+
+
+
+//: ---
+//: **4.** Do your best to describe the differences between a `class` and a `struct`.
+
+
+
+
+// no code needs to be written (verbal answer)
+
+
+
+
+//: ---
+/*: Pokemon
+ **5.** Create a custom type called Pokemon. There should be 6 properties associated with this type. The example below represents a `Dictionary` with its various key-value pairs. Taking a look at this `Dictionary`, gather what you can and name your properties along with figuring out the types of your properties on your `Pokemon` type. You will soon be tasked to create an `init` function on this custom type which will take in a `Dictionary` as its argument (like the example below).
+ ```swift
+ {
+ "id": 1,
+ "name": "bulbasaur",
+ "height": 7,
+ "weight": 69,
+ "abilities": {
+ "canHide": true,
+ "canTeleport": false }
+ }
+ ```
+ After creating your custom type called Pokemon. Create an `init` function on this type which takes in one argument called dictionary of type [`String` : `Any`]. Note that we haven't explained what this `Any` type means. We will assist you through this problem. This question is not to see how you can get to the right problem. It's more of us working with you to see how you think through the process of solving a problem you are somewhat familiar with.
+ 
+ This `init` function should parse through the `dictionary` argument passed in and look to assign values to each of the 6 stored properties. You should look to grab out the necessary info from the `dictionary` referencing back to the example above which will let you know what the various keys will look like that you can use.
+ */
+
+
+
+
+
+// write your code here (assisted by us)
+
+
+
+
+
